3aef77be5571c34fa1e9d477ba14203910fff088
Berke Space <stelberke@gmail.com>
2025-02-10 16:10:43 +0300

    Category picker changes

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Popover, PopoverTrigger } from "@/components/ui/popover";
 import { TransactionType } from "@/lib/types";
+import { Category } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 interface Props {
@@ -8,12 +11,44 @@ interface Props {
 }
 
 function CategoryPicker({ type }: Props) {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
   const categoriesQuery = useQuery({
     queryKey: ["categories", type],
     queryFn: () =>
       fetch(`/api/categories?type=${type}`).then((res) => res.json()),
   });
-  return <div>CategoryPicker</div>;
-}
 
+  const selectedCategory = categoriesQuery.data?.find(
+    (category: Category) => category.name === value
+  );
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {selectedCategory ? (
+            <CategoryRow category={selectedCategory} />
+          ) : (
+            "Kategori seçiniz"
+          )}
+        </Button>
+      </PopoverTrigger>
+    </Popover>
+  );
+}
 export default CategoryPicker;
+
+function CategoryRow({ category }: { category: Category }) {
+  return (
+    <div className="flex items-center gap-2">
+      <span role="img">{category.icon}</span>
+      <span>{category.name}</span>
+    </div>
+  );
+}
